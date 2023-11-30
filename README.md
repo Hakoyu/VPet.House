dbdcff8dcad76637b77b1fc4007b8a8a2e236db4
Hakoyu <Hakoyu@outlook.com>
2023-11-30 21:37:30 +0800

    添加项目文件。

---
+++ README.md
@@ -0,0 +1,12 @@
+# VPet.ModMaker
+
+《虚拟桌宠模拟器》MOD 制作器 0 门槛，打开就能用。支持所有游戏内容（动画，文本，效果皆可）。跟随游戏更新，出新内容，MOD 制作器也会跟随更新，立即上手制作吧！
+
+[游戏本体](https://store.steampowered.com/app/1920960)  
+[游戏本体Github](https://github.com/LorisYounger/VPet)  
+[MOD制作器](https://store.steampowered.com/app/2639250/VPet__MOD/)  
+[MOD制作器Github](https://github.com/LorisYounger/VPet.ModMaker)
+
+## [Wiki](https://github.com/LorisYounger/VPet.ModMaker/wiki)
+
+[简体中文Wiki](https://github.com/LorisYounger/VPet.ModMaker/wiki/%E7%AE%80%E4%BD%93%E4%B8%AD%E6%96%87%E4%B8%BB%E9%A1%B5%E9%9D%A2)
---
+++ VPet.House/Resources/NativeResources.cs
@@ -0,0 +1,62 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using System.IO;
+using System.Reflection;
+
+namespace VPet.House.Resources;
+
+/// <summary>
+/// 本地资源
+/// </summary>
+internal class NativeResources
+{
+    #region Resources
+
+    #endregion
+    /// <summary>
+    /// 资源基路径
+    /// </summary>
+    public const string ResourcePath = $"{nameof(VPet)}.{nameof(House)}.{nameof(Resources)}";
+
+    #region Native
+    private static readonly Assembly _assembly = Assembly.GetExecutingAssembly();
+
+    /// <summary>
+    /// 获取资源流
+    /// </summary>
+    /// <param name="resourceName">资源名</param>
+    /// <returns>资源流</returns>
+    public static Stream GetStream(string resourceName) =>
+        _assembly.GetManifestResourceStream(resourceName)!;
+
+    /// <summary>
+    /// 尝试获取资源流
+    /// </summary>
+    /// <param name="resourceName">资源名</param>
+    /// <param name="resourceStream">资源流</param>
+    /// <returns>成功为 <see langword="true"/> 失败为 <see langword="false"/></returns>
+    public static bool TryGetStream(string resourceName, out Stream resourceStream)
+    {
+        resourceStream = null;
+        if (_assembly.GetManifestResourceStream(resourceName) is not Stream stream)
+            return false;
+        resourceStream = stream;
+        return true;
+    }
+
+    /// <summary>
+    /// 将流保存至文件
+    /// </summary>
+    /// <param name="resourceName">资源名</param>
+    /// <param name="path">文件路径</param>
+    /// <returns>成功为 <see langword="true"/> 失败为 <see langword="false"/></returns>
+    public static bool SaveTo(string resourceName, string path)
+    {
+        if (_assembly.GetManifestResourceStream(resourceName) is not Stream stream)
+            return false;
+        using var sr = new StreamReader(stream);
+        using var sw = new StreamWriter(path);
+        sr.BaseStream.CopyTo(sw.BaseStream);
+        return true;
+    }
+    #endregion
+}
